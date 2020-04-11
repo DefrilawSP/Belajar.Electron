61f5d4535754a7b52cfbc1e770363fe60cc97bd3
unknown <defrisatrya@gmail.com>
2020-04-11 18:19:11 +0800

    Video ke 6 dan 7

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const electron = require("electron");
+const uuid = require("uuid").v4;
 
 const {
     app,
@@ -11,6 +12,7 @@ let todayWindow;
 let createWindow;
 let listWindow;
 
+let allAppointment = [];
 
 app.on("ready", () => {
     todayWindow = new BrowserWindow({
@@ -63,15 +65,21 @@ const createWindowCreator = () => {
 };
 
 ipcMain.on("appointment:create", (event, appointment) => {
-    console.log(appointment);
+    appointment["id"] = uuid();
+    appointment["done"] = 0;
+    allAppointment.push(appointment);
+
+    createWindow.close();
+
+    console.log(allAppointment);
 });
 ipcMain.on("appointment:request:list", event => {
-    console.log("here");
+    listWindow.webContents.send('appointment:response:list', allAppointment);
 });
 ipcMain.on("appointment:request:today", event => {
     console.log("here2");
 });
-ipcMain.on("appointment:done", (event, id) =>{
+ipcMain.on("appointment:done", (event, id) => {
     console.log("here3");
 });
 
